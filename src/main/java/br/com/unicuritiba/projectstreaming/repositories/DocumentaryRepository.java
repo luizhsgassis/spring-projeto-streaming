250d6dd3100702fd9f2b3aebba906bb67b95f0aa
Luiz Henrique <luizhsgassis.dev@gmail.com>
2025-04-29 20:08:16 -0300

    aula29_04

---
+++ src/main/java/br/com/unicuritiba/projectstreaming/repositories/DocumentaryRepository.java
@@ -0,0 +1,9 @@
+package br.com.unicuritiba.projectstreaming.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.unicuritiba.projectstreaming.models.Documentary;
+
+public interface DocumentaryRepository 
+	extends JpaRepository<Documentary, Long> {
+}
---
+++ src/main/java/br/com/unicuritiba/projectstreaming/repositories/SerieRepository.java
@@ -0,0 +1,9 @@
+package br.com.unicuritiba.projectstreaming.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.unicuritiba.projectstreaming.models.Serie;
+
+public interface SerieRepository
+	extends JpaRepository<Serie, Long> {
+}
